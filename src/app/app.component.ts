082e1eb81ad6f2fdf5fc920e6b2459c72674ef88
ealbanca <ealbanca@byui.edu>
2026-01-15 21:15:40 -0700

    Completed basic assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  showSecret = false;
+  log =[];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push( this.log.length + 1 );
+  }
 }
